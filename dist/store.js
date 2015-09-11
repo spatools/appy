c99df87d150ce73add9165a2d3578309cce4057c
Maxime LUCE <somatic@live.fr>
2015-09-11 05:43:29 +0200

    Update distribution

--- dist/store.js
+++ dist/store.js
@@ -112,7 +112,7 @@ define(["require", "exports"], function (require, exports) {
     })();
     exports.WebSQLStorage = WebSQLStorage;
     stores.websql = WebSQLStorage;
-    var indexedDB = win.indexedDB || win.mozIndexedDB || win.webkitIndexedDB || win.msIndexedDB || win.indexedDBShim, IDBTransaction = win.IDBTransaction || win.webkitIDBTransaction || win.msIDBTransaction || (win.indexedDBShim && win.indexedDBShim.modules.IDBTransaction), IDBKeyRange = win.IDBKeyRange || win.webkitIDBKeyRange || win.msIDBKeyRange || (win.indexedDBShim && win.indexedDBShim.modules.IDBKeyRange);
+    var indexedDB = win.indexedDB || win.mozIndexedDB || win.webkitIndexedDB || win.msIndexedDB || win.shimIndexedDB, IDBTransaction = win.IDBTransaction || win.webkitIDBTransaction || win.msIDBTransaction || (win.shimIndexedDB && win.shimIndexedDB.modules.IDBTransaction), IDBKeyRange = win.IDBKeyRange || win.webkitIDBKeyRange || win.msIDBKeyRange || (win.shimIndexedDB && win.shimIndexedDB.modules.IDBKeyRange);
     var IndexedDBStorage = (function () {
         function IndexedDBStorage() {
             this.db = null;
